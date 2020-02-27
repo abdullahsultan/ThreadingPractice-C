7f3c27e0cf621c20b57f6771491caddaaaeb79fc
abdullahsultan <abdullahsultan448@gmail.com>
2020-02-27 10:41:01 +0500

    done

--- T1.c
+++ T1.c
@@ -2,8 +2,29 @@
 #include<stdlib.h>
 #include<pthread.h>
 
+struct thread_data{
+    int time_arg;
+    char data;
+};
+
+void *my_thread(void* data)
+{
+  (thread_data*) data;
+  int x=0;
+  while(x<=100000)
+  {
+    printf("The Data:%c \n",data.data);
+    sleep(data.time_arg);
+  }
+}
+
+
+
 int main(int argc, char const *argv[]) {
-  long thread_id;
-  
+  thread_data t_data;
+  pthread_t threadID;
+  pthread_create(&threadID,NULL,my_thread,(void*)t_data);
+
+
   return 0;
 }
